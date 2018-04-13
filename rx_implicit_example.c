14a7c751d0562ca1f31404ca3da18a93d2c5f133
Ruslan <sul@gmail.com>
2018-04-13 10:32:12 +0300

    example updates

--- rx_implicit_example.c
+++ rx_implicit_example.c
@@ -2,6 +2,7 @@
 
 void rx_f(rxData *rx){
 printf("rx done \n");
+printf("CRC error: %d\n", rx->CRC);
 printf("string: %s\n", rx->buf);//Data we'v received
 printf("RSSI: %d\n", rx->RSSI);
 printf("SNR: %f\n", rx->SNR);
@@ -15,28 +16,24 @@ LoRa_ctl modem;
 modem.spiCS = 0;
 modem.rx.callback = rx_f;
 modem.rx.data.buf = rxbuf;
+modem.eth.payloadLen = 5;//payload len used in implicit header mode
 modem.eth.preambleLen=6;
-modem.eth.bw = BW62_5;
-modem.eth.sf = SF12;
-modem.eth.ecr = CR8;
-modem.eth.CRC = 1;
-modem.eth.freq = 434800000;
-modem.eth.resetGpioN = 4;
-modem.eth.dio0GpioN = 17;
-modem.eth.outPower = OP20;
-modem.eth.powerOutPin = PA_BOOST;
-modem.eth.AGC = 1;
-modem.eth.OCP = 240;
-modem.eth.payloadLen = 5;
-modem.eth.implicitHeader = 1;
+modem.eth.bw = BW62_5;//Bandwidth 62.5KHz
+modem.eth.sf = SF12;//Spreading Factor 12
+modem.eth.ecr = CR8;//Error coding rate CR4/8
+modem.eth.CRC = 1;//Turn on CRC checking
+modem.eth.freq = 434800000;// 434.8MHz
+modem.eth.resetGpioN = 4;//GPIO4 on lora RESET pi
+modem.eth.dio0GpioN = 17;//GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
+modem.eth.outPower = OP20;//Output power
+modem.eth.powerOutPin = PA_BOOST;//Power Amplifire pin
+modem.eth.AGC = 1;//Auto Gain Control
+modem.eth.OCP = 240;// 45 to 240 mA. 0 to turn off protection
+modem.eth.implicitHeader = 1;//Implicit header mode
 modem.eth.syncWord = 0x12;
 
 LoRa_begin(&modem);
-
 LoRa_receive(&modem);
-printf("Tsym: %f\n", modem.tx.data.Tsym);
-printf("Tpkt: %f\n", modem.tx.data.Tpkt);
-printf("payloadSymbNb: %u\n", modem.tx.data.payloadSymbNb);
 
 sleep(60);
 printf("end\n");
--- tx_implicit_example.c
+++ tx_implicit_example.c
@@ -13,24 +13,23 @@ modem.spiCS = 0;
 modem.tx.callback = tx_f;
 modem.tx.data.buf = txbuf;
 memcpy(modem.tx.data.buf, "LoRa", 5);//Data we'll sent
+modem.eth.payloadLen = 5;//Payload len in implicit header mode
 modem.eth.preambleLen=6;
-modem.eth.bw = BW62_5;
-modem.eth.sf = SF12;
-modem.eth.ecr = CR8;
-modem.eth.CRC = 1;
-modem.eth.freq = 434800000;
-modem.eth.resetGpioN = 4;
-modem.eth.dio0GpioN = 17;
-modem.eth.outPower = OP20;
-modem.eth.powerOutPin = PA_BOOST;
-modem.eth.AGC = 1;
-modem.eth.OCP = 240;
-modem.eth.payloadLen = 5;
-modem.eth.implicitHeader = 1;
+modem.eth.bw = BW62_5;//Bandwidth 62.5KHz
+modem.eth.sf = SF12;//Spreading Factor 12
+modem.eth.ecr = CR8;//Error coding rate CR4/8
+modem.eth.CRC = 1;//Turn on CRC checking
+modem.eth.freq = 434800000;// 434.8MHz
+modem.eth.resetGpioN = 4;//GPIO4 on lora RESET pi
+modem.eth.dio0GpioN = 17;//GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
+modem.eth.outPower = OP20;//Output power
+modem.eth.powerOutPin = PA_BOOST;//Power Amplifire pin
+modem.eth.AGC = 1;//Auto Gain Control
+modem.eth.OCP = 240;// 45 to 240 mA. 0 to turn off protection
+modem.eth.implicitHeader = 1;//implicit header mode
 modem.eth.syncWord = 0x12;
 
 LoRa_begin(&modem);
-
 LoRa_send(&modem);
 
 printf("Tsym: %f\n", modem.tx.data.Tsym);
